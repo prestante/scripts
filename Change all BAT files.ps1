4d55798954b43f48ab3471176d8742cdeca0b27d
prestante <prestante@yandex.ru>
2023-10-15 11:13:46 -0400

    Removed testing limitations

--- Change all BAT files.ps1
+++ Change all BAT files.ps1
@@ -1,11 +1,11 @@
-$sourceFiles = Get-ChildItem "\\wtlnas1.wtldev.net\Public\ADC\PS\bats Tecom" | Where-Object {$_.Name -match 'test.bat'}
+$sourceFiles = Get-ChildItem "\\wtlnas1.wtldev.net\Public\ADC\PS\bats Tecom" #| Select-Object -First 10
 
 foreach ($sourceFile in $sourceFiles) {
     $sourceFile.FullName
     $sourcePath = $sourceFile.FullName -replace '\\bats Tecom\\([\w\s!@#$%^&()\-=]+)\.bat','\scripts\$1.ps1'
     $batFill = "PowerShell -Command `"& '$sourcePath' some argument`""
     $destPath = $sourceFile.FullName -replace '\\bats Tecom\\','\bats\'
-    if (!(Get-Item $destPath)) {New-Item $destPath}
+    if (!(Get-Item $destPath -ea SilentlyContinue)) {New-Item $destPath | Out-Null}
     $batFill | Set-Content $destPath
 }
 
