bbdd1b13e1d8deebaab35920681b8826741c139e
prestante <prestante@yandex.ru>
2023-10-23 09:11:57 -0400

    Found a way to get both Timestamp_Sys100NS and PercentProcessorTime in one WMI Class, so now we can get CPU utilization in less than 100 ms

--- test WMI.ps1
+++ test WMI.ps1
@@ -6,10 +6,10 @@
 # Get Timestamp_Sys100NS
 # (Get-WmiObject -Query "SELECT Timestamp_Sys100NS FROM Win32_PerfRawData_PerfOS_System").Timestamp_Sys100NS
 
-#$table = Get-WmiObject -List | Where-Object {$_.Name -match 'processor'}
+#$table = Get-WmiObject -List | Where-Object {$_.Name -match 'system'}
 #$table | ft -AutoSize
 #Get-WmiObject Win32_Process | Where-Object {$_.Name -match 'TaskMgr'} #| select -Property Name, PercentProcessorTime, ElapsedTime
-#Get-WmiObject Win32_PerfFormattedData_Counters_ProcessorInformation | select -First 100 | select -Property  Name, Description, PercentProcessorTime, PercentProcessorUtility
+#Get-WmiObject Win32_PerfRawData_PerfProc_Process | select -First 1 | select -Property  Name, Description, PercentProcessorTime, PercentProcessorUtility
 
 function Get-RawResults {
     param (
@@ -31,30 +31,60 @@ function Get-FormattedResults {
 }
 function Get-TimeStamp {
     return (Get-WmiObject -Query "SELECT Timestamp_Sys100NS FROM Win32_PerfRawData_PerfOS_System").Timestamp_Sys100NS
+    #return (Get-WmiObject -Query "SELECT Timestamp_Sys100NS FROM Win32_PerfRawData_PerfProc_Process WHERE Name like 'Idle'").Timestamp_Sys100NS
+}
+function Get-MultiResults {
+    param (
+        [Parameter(Mandatory=0)][string]$procName,
+        [Parameter(Mandatory=0)][int]$procID
+    )
+    if ($procName) {return Get-WmiObject -Query "SELECT Timestamp_Sys100NS, PercentProcessorTime FROM Win32_PerfRawData_PerfProc_Process WHERE Name like '$procName'" | Select-Object -Property Timestamp_Sys100NS, PercentProcessorTime}
+    elseif ($procID) {return Get-WmiObject -Query "SELECT Timestamp_Sys100NS, PercentProcessorTime FROM Win32_PerfRawData_PerfProc_Process WHERE IDProcess like '$procID'" | Select-Object -Property Timestamp_Sys100NS, PercentProcessorTime}
+    else {Write-Error "You should give at least one of [string]procName or [int]procID"}
 }
 
-
-$procName = 'Idle'
-$procID = 0
+$procName = ''
+$procID = 3804
 $cores = (Get-WmiObject -Query "SELECT Name FROM Win32_PerfRawData_Counters_ProcessorInformation").Count - 2
 
-$timestampPrev = Get-TimeStamp
-$rawResultPrev = Get-RawResults -procName $procName -procID $procID
+#$timestampPrev = Get-TimeStamp
+#$rawResultPrev = Get-RawResults -procName $procName -procID $procID
+$rawResultsPrev = Get-MultiResults -procName $procName -procID $procID
 sleep 1  # for the first results to be more precise
 $formattedResult = 0
-$steps = 30
-for ($i = 0; $i -lt $steps; $i++) {
+do {
     $time0 = Get-Date
-    $timestamp = (Get-WmiObject -Query "SELECT Timestamp_Sys100NS FROM Win32_PerfRawData_PerfOS_System").Timestamp_Sys100NS
-    $rawResult = Get-RawResults -procName $procName -procID $procID
+    #$timestamp = Get-TimeStamp
+    #$rawResult = Get-RawResults -procName $procName -procID $procID
+    $rawResults = Get-MultiResults -procName $procName -procID $procID
     #$formattedResult = Get-FormattedResults -procName $procName -procID $procID
     $timeDiff = [int](((Get-Date) - $time0).TotalMilliseconds)
-    $resultRaw = [math]::Round((($rawResult - $rawResultPrev) / ($timestamp - $timestampPrev) / $cores * 100), 2)
+    #$resultRaw = [math]::Round((($rawResult - $rawResultPrev) / ($timestamp - $timestampPrev) / $cores * 100), 2)
+    $resultRaw = [math]::Round((($rawResults.PercentProcessorTime - $rawResultsPrev.PercentProcessorTime) / ($rawResults.Timestamp_Sys100NS - $rawResultsPrev.Timestamp_Sys100NS) / $cores * 100), 2)
     $resultFormatted = [math]::Round(($formattedResult / $cores), 2)
     $timestampPrev = $timestamp
     $rawResultPrev = $rawResult
     [pscustomobject]@{TimeDifference=$timeDiff; ResultFormatted=$resultFormatted; ResultRaw=$resultRaw; }
-    if ($timeDiff -lt 1000) {Start-Sleep -Milliseconds (1000 - $timeDiff)}
-}
+    if ($timeDiff -lt 999) {Start-Sleep -Milliseconds (999 - $timeDiff)}
+    
+    #looking for <Esc> or <R> or <Space> press
+    if ($host.ui.RawUi.KeyAvailable) {
+        $key=$host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown,IncludeKeyUp")
+        $host.UI.RawUI.FlushInputBuffer()
+        if ($key.keydown) {
+            switch ($key.VirtualKeyCode) {
+                <#C#> 67 {zero; if ($logging) {newLog}; if ($loggingSum) {newLogSum}}
+                <#L#> 76 {if ($logging) {$logging = 0} else {$logging = 1; newLog}}
+                <#M#> 77 {if ($loggingSum) {$loggingSum = 0} else {$loggingSum = 1; newLogSum}}
+                <#N#> 78 {newProcs; zero}
+                <#Enter#> 13 {}
+                <#Esc#> 27 {exit}
+                <#Space#> 32 {}
+                <#F1#> 112 {$infoCounter = 5} #to show help and peak cpu/mem time
+                <#F4#> 115 {$remCounter = 20} #to show abnormal counterResutls if any
+            } #end switch
+        }
+    } #end if
+} until ($key.VirtualKeyCode -eq 27)
 Write-Host ("Avg time diff: {0:n0} ms" -f ($avgTimeDiff/$steps))
 
