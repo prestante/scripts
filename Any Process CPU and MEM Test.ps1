69a3bf725ea9ee72c1d80491e0f1cf3580c0d6d6
prestante <prestante@yandex.ru>
2023-10-25 18:07:33 -0400

    Everything now works using ordered hash tables. Perf is pretty fast now. Fixed Avg/Low/Peak. Have to fix logging.

--- Any Process CPU and MEM Test.ps1
+++ Any Process CPU and MEM Test.ps1
@@ -23,12 +23,11 @@ function newLogSum {
 }
 function newProcs {
     do {
-        #Write-Host "Now you have to enter a Key Word corresponding to the Name or Description or PID of a processes to watch." -f Yellow -b Black
-        #Write-Host "Be careful with the Key Word. If some new process suitable for your Key Word will appear in the system later, it will be added to the list automatically." -f Yellow -b Black
-        #Write-Host "You can also type several keywords separated by comma. They all will be used at once to filter the processes list." -f Yellow -b Black
-        #Write-Host "Later you will be able to enter new Key Word by pressing <N>." -f Cyan -b Black
+        Write-Host "Now you have to enter a Key Word corresponding to the Name or Description or PID of a processes to watch." -f Yellow -b Black
+        Write-Host "Be careful with the Key Word. If some new process suitable for your Key Word will appear in the system later, it will be added to the list automatically." -f Yellow -b Black
+        Write-Host "You can also type several keywords separated by comma. They all will be used at once to filter the processes list." -f Yellow -b Black
+        Write-Host "Later you will be able to enter new Key Word by pressing <N>." -f Cyan -b Black
         #$Global:EnteredWords = Read-Host "Enter Key Word(s)"
-        #$Global:EnteredWords = 'ADC.Services'
         $Global:EnteredWords = 'edge'
         $Global:ProcKeyWords = $Global:EnteredWords -join '|'
         updProcs
@@ -44,7 +43,7 @@ function newProcs {
     } while ($agree -match 'n|N')
 }
 Function zero {
-    [int]$table.'Peak'.CPU = $table.'Peak'.Memory = $table.'Low'.CPU = $table.'Low'.Memory = 0
+    $Global:peakCpu = $Global:peakMem = $Global:lowCpu = $Global:lowMem = [decimal]0
     $Global:peakDateCpu = $Global:peakDateMem = $Global:lowDateCpu = $Global:lowDateMem = $null
     $Global:logging = $Global:loggingSum = $null
     $Global:startTime = Get-Date
@@ -52,82 +51,86 @@ Function zero {
 }
 function updProcs {
     $Global:timeGetProcs = Measure-Command {$Global:procs = get-process | Where-Object {$_.Id -match $Global:ProcKeyWords -or $_.Name -match $Global:ProcKeyWords}}
-    if ((($Global:table.Values.Id | Sort-Object) -join '') -ne (($Global:procs.Id | Sort-Object) -join '')) {  # old and new Table are different list has been changed
+    if ((($Global:table.Values.Id | Sort-Object) -join '') -ne (($Global:procs.Id | Sort-Object) -join '')) {  # old Table pricess IDs are not equal to newly found process IDs
         $Global:timeGetRawProcess = Measure-Command {
-            #$allRawProcesses = Get-WmiObject -Query "SELECT * FROM Win32_PerfRawData_PerfProc_Process WHERE NOT Name='_Total'"
-            $allRawProcesses = Get-WmiObject -Query "SELECT Name,IDProcess,PercentProcessorTime,WorkingSet,Timestamp_Sys100NS FROM Win32_PerfRawData_PerfProc_Process WHERE NOT Name='_Total'"
-        }
-        $Global:table = [ordered]@{}
-        $allRawProcesses | Where-Object {$_.Name -match $Global:ProcKeyWords} | ForEach-Object {
-            $Global:table.Add([string]$_.IDProcess, [PSCustomObject]@{
-                Name = $_.Name  -replace '^Harris.Automation.ADC.Services.' -replace 'Host' -replace 'Service' -replace 'Validation' -replace '#\d+$'
-                Id = $_.IDProcess
-                Memory = 0
-                CPU = 0
-                DecimalCPU = 0
-                Start = (Get-Process -Id $_.IDProcess -ea SilentlyContinue).StartTime  # looks like it doesn't affect the performance, however it sometimes leads to errors
-                LastPercentProcessorTime = $_.PercentProcessorTime
-                LastWorkingSet = $_.WorkingSet
-                LastTimestamp_Sys100NS = $_.Timestamp_Sys100NS
-            })
-        }
-        if (-not $Global:table.Values.Id) {$Global:table.Add($Global:ProcKeyWords, [PSCustomObject]@{
-            Name = "$Global:ProcKeyWords"
-            Id = "N/A"
-            Memory = 0
-            CPU = 0})
-            $Global:logging = $Global:loggingSum = $null}
-        $Global:table.Add('Divider', [PSCustomObject]@{Name = '---------------'})
-        $Global:table.Add('Sum', [pscustomobject]@{Name = 'Sum'; Memory = 0; CPU = 0; DecimalCPU = 0})
-        $Global:table.Add('Space1', [PSCustomObject]@{})
-        $Global:table.Add('Peak', [pscustomobject]@{Name = 'Peak'; Memory = 0; CPU = 0})
-        $Global:table.Add('Average', [pscustomobject]@{Name = 'Average'; Memory = 0; CPU = 0; DecimalCPU = 0})
-        $Global:table.Add('Low', [pscustomobject]@{Name = 'Low'; Memory = 0; CPU = 0})
-        $Global:table.Add('Space2', [PSCustomObject]@{})
-        $allRawProcesses | Where-Object {$_.Name -eq 'Idle'} | ForEach-Object {
+            $Global:table = [ordered]@{}
+            #$allRawProcesses | Where-Object {$_.Name -match $Global:ProcKeyWords} | ForEach-Object {
+            Get-WmiObject -Query "SELECT Name,IDProcess,PercentProcessorTime,WorkingSet,Timestamp_Sys100NS FROM Win32_PerfRawData_PerfProc_Process WHERE NOT Name='_Total'" | Where-Object {$_.Name -match "$Global:ProcKeyWords|Idle"} | ForEach-Object {
+                if ($_.Name -ne 'Idle') {
+                    $Global:table.Add([string]$_.IDProcess, [PSCustomObject]@{
+                        Name = [string]$_.Name  -replace '^Harris.Automation.ADC.Services.' -replace 'Host' -replace 'Service' -replace 'Validation' -replace '#\d+$'
+                        Id = [int]$_.IDProcess
+                        Memory = [int]0
+                        CPU = [int]0
+                        DecimalCPU = [decimal]0
+                        Start = (Get-Process -Id $_.IDProcess -ea SilentlyContinue).StartTime  # looks like it doesn't affect the performance, however it sometimes leads to errors
+                        LastPercentProcessorTime = [UInt64]$_.PercentProcessorTime
+                        LastWorkingSet = [UInt64]$_.WorkingSet
+                        LastTimestamp_Sys100NS = [UInt64]$_.Timestamp_Sys100NS
+                    })
+                } else {$idleLastPercentProcessorTime = [UInt64]$_.PercentProcessorTime; $idleLastTimestamp_Sys100NS = [UInt64]$_.Timestamp_Sys100NS}  # saving Idle process CPU for later using in TOTAL
+            }
+            if (-not $Global:table.Values.Id) {$Global:table.Add($Global:ProcKeyWords, [PSCustomObject]@{  # when no processes found by key words
+                Name = "$Global:ProcKeyWords"
+                Id = "N/A"
+                Memory = [int]0
+                CPU = [int]0})
+                $Global:logging = $Global:loggingSum = $null
+            }
+            $Global:table.Add('Divider', [PSCustomObject]@{Name = '---------------'})
+            $Global:table.Add('Sum', [pscustomobject]@{Name = 'Sum'; Memory = [int]0; CPU = [int]0; DecimalCPU = [decimal]0})
+            $Global:table.Add('Space1', [PSCustomObject]@{})
+            $Global:table.Add('Peak', [pscustomobject]@{Name = 'Peak'; Memory = [int]0; CPU = [int]0})
+            $Global:table.Add('Average', [pscustomobject]@{Name = 'Average'; Memory = [int]0; CPU = [int]0; DecimalCPU = [decimal]0})
+            $Global:table.Add('Low', [pscustomobject]@{Name = 'Low'; Memory = [int]0; CPU = [int]0})
+            $Global:table.Add('Space2', [PSCustomObject]@{})
             $Global:table.Add('TOTAL', [PSCustomObject]@{
                 Name = 'TOTAL'
-                LastMemory = 0
-                Memory = 0
-                LastCPU = 0
-                CPU = 0
-                LastPercentProcessorTime = $_.PercentProcessorTime
-                LastTimestamp_Sys100NS = $_.Timestamp_Sys100NS
-            })
-        }        
-        if ($Global:logging) {newLog}
-        if ($Global:loggingSum) {newLogSum}
+                LastMemory = [int]0
+                Memory = [int]0
+                LastCPU = [int]0
+                CPU = [int]0
+                LastPercentProcessorTime = $idleLastPercentProcessorTime
+                LastTimestamp_Sys100NS = $idleLastTimestamp_Sys100NS
+            })        
+            if ($Global:logging) {newLog}
+            if ($Global:loggingSum) {newLogSum}
+        }
     }
 }
 Function updCounters {
     $Global:timeAllRawProcess = Measure-Command {
-        $allRawProcesses = Get-WmiObject -Query "SELECT * FROM Win32_PerfRawData_PerfProc_Process WHERE NOT Name='_Total'"  # Getting all processes raw information (except _Total because its Id equals 0 and equals Idle which is also 0)
+        $allRawProcesses = [ordered]@{}
+        Get-WmiObject -Query "SELECT Name,IDProcess,PercentProcessorTime,WorkingSet,Timestamp_Sys100NS FROM Win32_PerfRawData_PerfProc_Process WHERE NOT Name='_Total'" | ForEach-Object {
+            $allRawProcesses.Add([string]$_.IDProcess, [PSCustomObject]@{
+                Name = [string]$_.Name
+                IDProcess = [int]$_.IDProcess
+                PercentProcessorTime = [uint64]$_.PercentProcessorTime
+                WorkingSet = [uint64]$_.WorkingSet
+                Timestamp_Sys100NS = [uint64]$_.Timestamp_Sys100NS
+            })
+        }
     }
     
-    # There is still a way to speed up the process by converting entire $allRawProcesses to the hash table like it is done in updProcs and then get its value very fast
-    #$RawProcesses = @()  # I think we don't need this
-    #foreach ($id in ($Global:Processes.IdProcess)) {$allRawProcesses | Where-Object {$_.IDProcess -eq $id} | ForEach-Object {$RawProcesses += $_}}  # the longest part
     $Global:table."Sum".Memory = $Global:table."Sum".CPU = $Global:table."Sum".DecimalCPU = [decimal]0
     $Global:timeToUpdateTable = Measure-Command {
-        <#foreach ($id in $Global:table.Values.Id) {
-            if ($id) {
-                $currentRawProc = $allRawProcesses.where({$_.IDProcess -eq $id})
-                $currentTableProc = $Global:table."$id"  # for avoiding double $_ $_ in one line
-                $currentTableProc.Memory = [int]($currentRawProc.WorkingSet/1mb)
-                $currentTableProc.DecimalCPU = ($currentRawProc.PercentProcessorTime - $currentTableProc.LastPercentProcessorTime) / ($currentRawProc.Timestamp_Sys100NS - $currentTableProc.LastTimestamp_Sys100NS) / $Global:LogicalCPUs * 100
-                $currentTableProc.CPU = [int]$currentTableProc.DecimalCPU
-                $currentTableProc.LastWorkingSet = $currentRawProc.WorkingSet
-                $currentTableProc.LastPercentProcessorTime = $currentRawProc.PercentProcessorTime
-                $currentTableProc.LastTimestamp_Sys100NS = $currentRawProc.Timestamp_Sys100NS
-                $Global:table.'Sum'.Memory += $currentTableProc.Memory
-                $Global:table.'Sum'.DecimalCPU += $currentTableProc.DecimalCPU
+        foreach ($id in $Global:table.Values.Id) {
+            if ($id) {  # there are fields without Id like Sum/Low/Avg/Peak/Total and --------- dividers
+                $Global:table."$id".Memory = [int]($allRawProcesses."$id".WorkingSet/1mb)
+                $Global:table."$id".DecimalCPU = ($allRawProcesses."$id".PercentProcessorTime - $Global:table."$id".LastPercentProcessorTime) / ($allRawProcesses."$id".Timestamp_Sys100NS - $Global:table."$id".LastTimestamp_Sys100NS) / $Global:LogicalCPUs * 100
+                $Global:table."$id".CPU = [int]$Global:table."$id".DecimalCPU
+                $Global:table."$id".LastWorkingSet = $allRawProcesses."$id".WorkingSet
+                $Global:table."$id".LastPercentProcessorTime = $allRawProcesses."$id".PercentProcessorTime
+                $Global:table."$id".LastTimestamp_Sys100NS = $allRawProcesses."$id".Timestamp_Sys100NS
+                $Global:table.'Sum'.Memory += $Global:table."$id".Memory
+                $Global:table.'Sum'.DecimalCPU += $Global:table."$id".DecimalCPU
             }
-        }#>
-        #$Global:table."Sum".CPU = [int]$Global:table.'Sum'.DecimalCPU
-        #$Global:table.'TOTAL'.CPU = [int](100-[math]::Floor(($allRawProcesses.where({$_.Name -match 'Idle'}).PercentProcessorTime - $Global:table.'TOTAL'.LastPercentProcessorTime) / ($allRawProcesses.where({$_.Name -match 'Idle'}).Timestamp_Sys100NS - $Global:table.'TOTAL'.LastTimestamp_Sys100NS) / $Global:LogicalCPUs * 100))
-        #$Global:table.'TOTAL'.Memory = [int]($Global:totalMemory - (Get-WmiObject Win32_PerfRawData_PerfOS_Memory).AvailableBytes/1mb)
-        #$Global:table.'TOTAL'.LastPercentProcessorTime = $allRawProcesses.where({$_.Name -match 'Idle'}).PercentProcessorTime
-        #$Global:table.'TOTAL'.LastTimestamp_Sys100NS = $allRawProcesses.where({$_.Name -match 'Idle'}).Timestamp_Sys100NS
+        }
+        $Global:table."Sum".CPU = [int]$Global:table.'Sum'.DecimalCPU
+        $Global:table.'TOTAL'.CPU = [int](100-[math]::Floor(($allRawProcesses.'0'.PercentProcessorTime - $Global:table.'TOTAL'.LastPercentProcessorTime) / ($allRawProcesses.'0'.Timestamp_Sys100NS - $Global:table.'TOTAL'.LastTimestamp_Sys100NS) / $Global:LogicalCPUs * 100))
+        $Global:table.'TOTAL'.Memory = [int]($Global:totalMemory - (Get-WmiObject Win32_PerfRawData_PerfOS_Memory).AvailableBytes/1mb)
+        $Global:table.'TOTAL'.LastPercentProcessorTime = $allRawProcesses.'0'.PercentProcessorTime
+        $Global:table.'TOTAL'.LastTimestamp_Sys100NS = $allRawProcesses.'0'.Timestamp_Sys100NS
     }
 }
 
@@ -150,15 +153,18 @@ do {
     $Global:timePeakLowAvg = Measure-Command {
         if ($table.'Sum'.CPU + $table.'Sum'.Memory -eq 0) {$zeroFlag++} else {$zeroFlag=0}
         if (($zeroFlag -eq 5) -or (($table.'Sum'.CPU+$table.'Sum'.Memory -eq 0) -and ((Get-Date)-($startTime)).TotalSeconds -le 5)) {$zeroFlag=0 ; zero}
-        [int]$table.'Peak'.CPU = if ($table.'Peak'.CPU -lt $table.'Sum'.CPU) {$table.'Sum'.CPU; $peakDateCpu = Get-Date} else {$table.'Peak'.CPU}
-        [int]$table.'Peak'.Memory = if ($table.'Peak'.Memory -lt $table.'Sum'.Memory) {$table.'Sum'.Memory; $peakDateMem = Get-Date} else {$table.'Peak'.Memory}
-        [int]$table.'Low'.CPU = if ($table.'Low'.CPU -gt $table.'Sum'.CPU) {$table.'Sum'.CPU; $lowDateCpu = Get-Date} else {$table.'Low'.CPU}
-        [int]$table.'Low'.Memory = if (($table.'Low'.Memory -gt $table.'Sum'.Memory) -or ($table.'Low'.Memory -eq 0)) {$table.'Sum'.Memory; $lowDateMem = Get-Date} else {$table.'Low'.Memory}
-        [double]$table.'Average'.DecimalCPU = ($table.'Average'.DecimalCPU * $qt + $table.'Sum'.DecimalCPU) / ($qt + 1)
-        [int]$table.'Average'.CPU = $table.'Average'.DecimalCPU
-        $temp = $table.'Average'.Memory
-        [int]$table.'Average'.Memory = ($temp * $qt + $table.'Sum'.Memory) / ($qt + 1)
-        #[int]$table.'Average'.Memory = ($table.'Average'.Memory * $qt + $table.'Sum'.Memory) / ($qt + 1)
+        $peakCpu = if ($peakCpu -lt $table.'Sum'.CPU) {$table.'Sum'.CPU; $peakDateCpu = Get-Date} else {$peakCpu}
+        [int]$table.'Peak'.CPU = $peakCPU
+        $peakMem = if ($peakMem -lt $table.'Sum'.Memory) {$table.'Sum'.Memory; $peakDateMem = Get-Date} else {$peakMem}
+        [int]$table.'Peak'.Memory = $peakMem
+        $lowCpu = if ($lowCpu -gt $table.'Sum'.CPU) {$table.'Sum'.CPU; $lowDateCpu = Get-Date} else {$lowCpu}
+        [int]$table.'Low'.CPU = $lowCPU
+        $lowMem = if (($lowMem -gt $table.'Sum'.Memory) -or ($lowMem -eq 0)) {$table.'Sum'.Memory; $lowDateMem = Get-Date} else {$lowMem}
+        [int]$table.'Low'.Memory = $lowMem
+        [double]$avgCpu = ($avgCpu * $qt + $table.'Sum'.DecimalCPU) / ($qt + 1)
+        [int]$table.'Average'.CPU = $avgCpu
+        $avgMem = ($avgMem * $qt + $table.'Sum'.Memory) / ($qt + 1)
+        [int]$table.'Average'.Memory = $avgMem
         $qt++
     }
 
