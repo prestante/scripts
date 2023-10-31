68ccbcbfe32439d196f51e79850577d5a5130c9c
prestante <prestante@yandex.ru>
2023-10-31 11:50:07 -0400

    test push changes from galk after made them in adcs

--- test.ps1
+++ test.ps1
@@ -1,8 +1,1 @@
-﻿# Trying to convert giant WMI array to hash table
-$i = 0
-do {
-    $spent = [int](Measure-Command {
-        $count = (Get-WmiObject -Query "SELECT Name,IDProcess,PercentProcessorTime,WorkingSet,Timestamp_Sys100NS FROM Win32_PerfRawData_PerfProc_Process WHERE NOT Name='_Total'").Count
-    }).TotalMilliseconds
-    Write-Host "Spent: $spent `t Count: $count `t Iterations: $i"
-} While (1) #((Read-Host) -ne 'n')
+﻿#trying to save changes from ADCS
