8fb879c73c06e73b5fe82c40c1703ef5a257a186
prestante <prestante@yandex.ru>
2023-11-01 11:45:10 -0400

    Another rework of Remove Run Warnings

--- Remove Run Warning.ps1
+++ Remove Run Warning.ps1
@@ -1,17 +1,11 @@
-﻿$address = 'wtlnas1.wtldev.net'
-$domain = $address -replace '.*\.(\w+\.\w+$)','$1'
-$place = $address -replace '(.*)\.\w+\.\w+$','$1'
-$registryPaths = @()
-$registryPaths += "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\EscDomains"
-$registryPaths += "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains"
+﻿$addresses = @('wtlnas1','wtlnas5')
+$registryPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\EscDomains"
 
-$name2 = "file"
-$value2 = 1
+$name = "*"
+$value = 1
 
-foreach ($registryPath in $registryPaths) {
-  if (!(Test-Path $registryPath)) {New-Item -Path $registryPath -Force}
-  if (!(Test-Path "$registryPath\$domain")) {New-Item -Path "$registryPath\$domain" -Force}
-  if (!(Test-Path "$registryPath\$domain\$place")) {New-Item -Path "$registryPath\$domain\$place" -Force}
-  if ((Get-Item "$registryPath\$domain\$place").property -notcontains $name2) {New-ItemProperty -Path "$registryPath\$domain\$place" -Name $name2 -Value $value2 -PropertyType DWORD -Force}
-}
+foreach ($address in $addresses) {
+    if (-not (Test-Path "$registryPath\$address")) {New-Item -Path "$registryPath\$address" -Force}
+    if (-not (Get-Item "$registryPath\$address" -ea SilentlyContinue).Property -contains $name) {New-ItemProperty -Path "$registryPath\$address" -Name $name -Value $value -PropertyType DWORD -Force}
+    Get-Item "$registryPath\$address" -ea SilentlyContinue}
  
--- test.ps1
+++ test.ps1
@@ -1,1 +1,11 @@
-﻿#trying to save changes from ADCS
+﻿$addresses = @('wtlnas1','wtlnas5')
+$registryPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\EscDomains"
+
+$name = "*"
+$value = 1
+
+foreach ($address in $addresses) {
+    if (-not (Test-Path "$registryPath\$address")) {New-Item -Path "$registryPath\$address" -Force}
+    if (-not (Get-Item "$registryPath\$address" -ea SilentlyContinue).Property -contains $name) {New-ItemProperty -Path "$registryPath\$address" -Name $name -Value $value -PropertyType DWORD -Force}
+    Get-Item "$registryPath\$address" -ea SilentlyContinue}
+ 
