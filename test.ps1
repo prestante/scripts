57a91e40a16588cc13c4e20582fa2d30f1218ad8
Alexander Galkovsky <prestante@yandex.ru>
2024-03-19 03:26:42 -0400

    last changes before trying to send BXFs to CTC

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
 ﻿#$CTC = @('WTL-ADC-CTC-01.wtldev.net', 'WTL-ADC-CTC-02.wtldev.net', 'WTL-ADC-CTC-03.wtldev.net', 'WTL-ADC-CTC-04.wtldev.net', 'WTL-ADC-CTC-05.wtldev.net', 'WTL-ADC-CTC-06.wtldev.net', 'WTL-ADC-CTC-07.wtldev.net', 'WTL-ADC-CTC-08.wtldev.net', 'WTL-ADC-CTC-09.wtldev.net', 'WTL-ADC-CTC-10.wtldev.net', 'WTL-ADC-CTC-11.wtldev.net', 'WTL-ADC-CTC-12.wtldev.net', 'WTL-ADC-CTC-13.wtldev.net', 'WTL-ADC-CTC-14.wtldev.net', 'WTL-ADC-CTC-15.wtldev.net', 'WTL-ADC-CTC-16.wtldev.net', 'WTL-ADC-CTC-17.wtldev.net', 'WTL-ADC-CTC-18.wtldev.net', 'WTL-ADC-CTC-19.wtldev.net', 'WTL-ADC-CTC-20.wtldev.net', 'WTL-ADC-CTC-21.wtldev.net', 'WTL-ADC-CTC-22.wtldev.net', 'WTL-ADC-CTC-23.wtldev.net', 'WTL-ADC-CTC-24.wtldev.net', 'WTL-ADC-CTC-25.wtldev.net', 'WTL-ADC-CTC-26.wtldev.net', 'WTL-ADC-CTC-27.wtldev.net', 'WTL-ADC-CTC-28.wtldev.net', 'WTL-ADC-CTC-29.wtldev.net', 'WTL-ADC-CTC-30.wtldev.net', 'WTL-ADC-CTC-31.wtldev.net', 'WTL-ADC-CTC-32.wtldev.net')
-$CTC = @('WTL-ADC-CTC-01.wtldev.net')
+$CTC = @('WTL-ADC-CTC-01.wtldev.net', 'WTL-ADC-CTC-02.wtldev.net')
 $WannaRemove = 0  # top priority
 $InstallAppVersion = '5.9.10.1'
 $DBparams = @{DBName = 'wtl-hpx-325-m01' ; DBUser = 'sa' ; DBPassword = 'ImagineDB1'}
@@ -33,24 +33,48 @@ Invoke-Command -ComputerName $CTC -ArgumentList $InstallAppVersion, $CredsLocal,
         return $Report
     }
     function Copy-Installer {
+        param ( $Version = $InstallAppVersion)
         $Report = ''
-        try { if (-not (Get-ChildItem B: -ErrorAction Stop | Where-Object { $_.Name -match $InstallAppVersion }).Count) { Throw }
-            $DistantFolderPath = ( Get-ChildItem B: -ErrorAction Stop | Where-Object { $_.Name -match $InstallAppVersion } | Select-Object -First 1 ).FullName
+        try { if (-not (Get-ChildItem B: -ErrorAction Stop | Where-Object { $_.Name -match $Version }).Count) { Throw }
+            $DistantFolderPath = ( Get-ChildItem B: -ErrorAction Stop | Where-Object { $_.Name -match $Version } | Select-Object -First 1 ).FullName
             try { $DistantFilePath = (Get-ChildItem $DistantFolderPath -ErrorAction Stop | Where-Object { $_.Name -match '^ADCServicesSetup.*exe' } | Select-Object -First 1).FullName
                 try { Test-Path $DistantFilePath -ErrorAction Stop | Out-Null
                     try { New-Item "C:\temp" -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
-                        try { $Report += "`n`t Copy ADC Services $InstallAppVersion installer into C:\temp - "
+                        try { $Report += "`n`t Copy ADC Services $Version installer into C:\temp - "
                             Copy-Item $DistantFilePath "C:\temp\" -ErrorAction Stop
                             $Report += "Done"
                         } catch { $Report += "`n`t Error while copying the installer to 'C:\temp': $_" }
                     } catch { $Report += "`n`t Error while creating C:\temp directory: $_" }
                 } catch { $Report += "`n`t Error while looking for the desired installer in '$DistantFolderPath': $_" }
             } catch { $Report += "`n`t Error while getting files from '$DistantFolderPath': $_" }
-        } catch { $Report += "`n`t Error while searching for the appropriate folder for version $InstallAppVersion" }
+        } catch { $Report += "`n`t Error while searching for the appropriate folder for version $Version" }
+        return $Report
+    }
+    function Stop-Services {
+        $Services = Get-Service -Name 'ADC*' -ErrorAction SilentlyContinue
+        if ( $Services.Status -contains 'Running' ) {  # Some ADC Services are running, stopping them
+            $Report = "`n`t Stopping ADC Services - "
+            $Services | Set-Service -StartupType Disabled -ErrorAction SilentlyContinue ; Start-Sleep 1
+            Get-Process -Name 'Harris.Automation.ADC.Services*' -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue ; Start-Sleep 1
+            Get-Process -Name 'Harris.Automation.ADC.Services*' -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue ; Start-Sleep 1
+            $Services  | Set-Service -StartupType Manual -ErrorAction SilentlyContinue ; Start-Sleep 1
+            $Report += "Done"
+        }        
         return $Report
     }
     function Install-Services {
         $Report = ''
+        if (!(Get-ItemProperty HKLM:Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -like "Microsoft SQL Server Compact*"} | Select-Object DisplayName, DisplayVersion)) {
+            try {
+                $File = Copy-Item 'P:\Resources\SSCERuntime_x64-ENU.exe' 'C:\temp' -PassThru
+                $Report += "`n`t Installing Microsoft SQL Server Compact 4.0 SP1 x64 ENU - "
+                Start-Process $File.FullName -ArgumentList "/package /quiet" -Wait
+                $Report += "Done"
+            }
+            catch {
+                $Report += "Fail"
+            }
+        }
         $File = Get-ChildItem 'C:\temp' | Where-Object { $_.Name -match 'ADCServicesSetup' -and $_.VersionInfo.ProductVersion -match $InstallAppVersion} | Select-Object -Last 1
         if ( $File ) {  # If there is an installer, prepare parameters and start removal
             $DBName = $DBparams.DBName ; $DBUser = $DBparams.DBUser ; $DBPassword = $DBparams.DBPassword
@@ -58,22 +82,15 @@ Invoke-Command -ComputerName $CTC -ArgumentList $InstallAppVersion, $CredsLocal,
             $Report += "`n`t Installing $($File.VersionInfo.ProductName) $($File.VersionInfo.ProductVersion) - "
             Start-Process $File.FullName -ArgumentList $Parameters -Wait
             $Report += "Done"
+            $Report += Stop-Services
         }
         else { $Report += "Fail`n`t Cannot install ADC Services $($InstalledApp.DisplayVersion) because there is no appropriate installer in C:\temp"}
         return $Report
     }
     function Remove-Services {
         $Report = ''
-        $Services = Get-Service -Name 'ADC*' -ErrorAction SilentlyContinue
-        if ($Services) {
-            if ( $Services.Status -contains 'Running' ) {  # Some ADC Services are running, stopping them
-                $Report += "`n`t Stopping ADC Services - "
-                $Services | Set-Service -StartupType Disabled -ErrorAction SilentlyContinue ; Start-Sleep 1
-                Get-Process -Name 'Harris.Automation.ADC.Services*' -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue ; Start-Sleep 1
-                Get-Process -Name 'Harris.Automation.ADC.Services*' -ErrorAction SilentlyContinue | Stop-Process -Force -ErrorAction SilentlyContinue ; Start-Sleep 1
-                $Services  | Set-Service -StartupType Manual -ErrorAction SilentlyContinue ; Start-Sleep 1
-                $Report += "Done"
-            }
+        if (Get-Service -Name 'ADC*' -ErrorAction SilentlyContinue) {
+            $Report += Stop-Services
             $File = Get-ChildItem 'C:\temp' | Where-Object { $_.Name -match 'ADCServicesSetup' -and $_.VersionInfo.ProductVersion -match $InstallAppVersion} | Select-Object -Last 1
             if ( $File ) {  # If there is an installer, prepare parameters and start removal
                 $Parameters = "/x /s, /v`"/qn`" /v`"DELETEDB=No`" /v`"REBOOT=ReallySuppress`"" # parameters for removing ADC Services
@@ -83,22 +100,22 @@ Invoke-Command -ComputerName $CTC -ArgumentList $InstallAppVersion, $CredsLocal,
             }
             else { $Report += "`n`t Cannot remove ADC Services $($InstalledApp.DisplayVersion) because there is no appropriate installer in C:\temp"}
         }
-        else { $Report += "`n`t Cannot remove ADC Services $($InstalledApp.DisplayVersion) because ADC Services are not found in a system"}
+        else { $Report += "`n`t Cannot remove ADC Services $($InstalledApp.DisplayVersion) because ADC Services are not found in the system"}
         return $Report
     }
     function Set-Configs {
-        # copy ref files to C:\temp       TRY TO CREATE CONFIG FILES READING AND CHANGING THEM ON THE FLY
+        $Report = "`n`t Copying ADC Services config files, rules and license - "
         Get-ChildItem 'P:\resources\CTC Services Refs' | Where-Object {$_.Name -match '.*CTCRef.xml'} | ForEach-Object { 
             (Get-Content $_.FullName) -replace '#NUM',$HostName.Replace('WTL-ADC-CTC-','') -replace '#ADDRESS',$IPaddress -replace '#DBName',$DBparams.DBName -replace '#DBUser',$DBparams.DBUser -replace '#DBPassword',$DBparams.DBPassword |
             Out-File "C:\Program Files (x86)\Imagine Communications\ADC Services\config\$($_.Name -replace 'CTCRef')"  -Encoding utf8 -Force
         }
-        # create rules and license directories  TRY TO CREATE FOLDERS BY THE SAME COMMAND AS COPY FILES
-        #New-Item -Path 'C:\Program Files (x86)\Imagine Communications\ADC Services\rules' -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
-        #New-Item -Path 'C:\Program Files (x86)\Imagine Communications\ADC Services\license' -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
-        # copy license, rule and IntegrationServiceHost.exe.config
+        New-Item -Path 'C:\Program Files (x86)\Imagine Communications\ADC Services\rules' -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
+        New-Item -Path 'C:\Program Files (x86)\Imagine Communications\ADC Services\license' -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
         Copy-Item 'P:\resources\ADCLicense.lic' 'C:\Program Files (x86)\Imagine Communications\ADC Services\license' -Force
         Copy-Item 'P:\resources\PlaylistTranslator.Rule.xml' 'C:\Program Files (x86)\Imagine Communications\ADC Services\rules' -Force
         Copy-Item 'P:\resources\Harris.Automation.ADC.Services.IntegrationServiceHost.exe.config' 'C:\Program Files (x86)\Imagine Communications\ADC Services' -Force
+        $Report += "Done"
+        return $Report
     }
 
     # check which ADC Services are already installed, if any
@@ -113,16 +130,18 @@ Invoke-Command -ComputerName $CTC -ArgumentList $InstallAppVersion, $CredsLocal,
         $Report += Set-Drive
         $Report += Copy-Installer
         $Report += Install-Services
+        $Report += Set-Configs
     }
     elseif ( $InstalledApp.DisplayVersion -match $InstallAppVersion ) {  # we already have the same version installed
         $Report += "`n`t $($InstalledApp.DisplayName) $($InstalledApp.DisplayVersion) is already installed."
+        $Report += Set-Configs
     }
     elseif ( $InstallAppVersion -notmatch $InstallAppVersion ) {  # we have other version installed, so try to upgrade or find, copy and run a corresponding installer to delete
         $Report += "`n`t $($InstalledApp.DisplayName) $($InstalledApp.DisplayVersion) is installed, upgrading to $($InstallAppVersion) - "
+        $Report += Set-Configs
     } 
     else { $Report += "`n`t Don't know what else))" }
 
-    Set-Configs
 
     #$Report += "`n`t Returning so far"
     Write-Host "$Report" -f ( 1, 2, 3, 5, 6, 9, 10, 11, 13, 14 )[ ( $HostName.Split('-')[-1] ) % 10 ]  # Choose the color as a remainder of dividing the name number part by 10 (number of color variants)
