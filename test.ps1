fe3a5cf0dea03db57ad5d9908b8a38d3e2137a99
Alexander Galkovsky <prestante@yandex.ru>
2025-03-07 23:41:07 +0100

    testing ssh using powershell on mac

--- test.ps1
+++ test.ps1
@@ -1,3 +1,17 @@
-﻿Invoke-Command -ComputerName 'adc-ctc01.tecomgroup.ru' -ScriptBlock {
+﻿$CTC = @('WTL-ADC-CTC-01.wtldev.net', 'WTL-ADC-CTC-02.wtldev.net')
+
+$CredsDomain = [System.Management.Automation.PSCredential]::new('wtldev.net\vadc',(ConvertTo-SecureString -AsPlainText $env:VADC_PASSWORD -Force))
+
+$add = 0
+
+Invoke-Command -ComputerName ($CTC) -InDisconnectedSession -ArgumentList $add -Credential $CredsDomain {
+    param ($add)
+    if ($add -eq 0) {
+        Stop-Process  -name ADC1000NT -Force ; Start-Sleep 1
+        Start-Process 'C:\Users\Public\Desktop\ADC Device Server.lnk' ; Start-Sleep 1
+    }
+}
+
+Invoke-Command -ComputerName ($CTC) -Credential $CredsDomain {
     Get-Process -Name 'ADC1000NT'
 }
