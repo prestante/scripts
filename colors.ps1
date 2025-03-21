052b50c9860947720b53fb5649a07f233d7a26df
prestante <prestante@yandex.ru>
2025-03-21 11:39:20 +0000

    minor changes

--- colors.ps1
+++ colors.ps1
@@ -2,6 +2,9 @@
     Write-Host ("{0:d2}" -f $i) -NoNewline
     Write-Host "                         " -b $i
 }
-Read-Host
+# Ask for input if not in the IDE
+if ($Host.Name -notlike "*Code*") {
+    Read-Host "Press Enter to continue..."
+}
 
 #write-host "Test string with numbers 123456 --------- 23259195. OK?" -f 13 -b 5
